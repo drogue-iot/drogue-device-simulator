88f84b7cb558cad800777d6148692daa62364492
ctron <ctron@users.noreply.github.com>
2022-03-22 12:10:08 +0000

    deploy: 05e17118affc92c2bb94c8605c9111173100a767

--- index-c09968cd808c9fc9.js
+++ index-c09968cd808c9fc9.js
@@ -360,7 +360,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-c25241daccaf2840_bg.wasm', import.meta.url);
+        input = new URL('index-c09968cd808c9fc9_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
