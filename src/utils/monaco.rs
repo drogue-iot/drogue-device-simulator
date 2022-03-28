9f7d1fd6d22b97b753cfde7dbe7ac6760df003dd
Jens Reimann <jreimann@redhat.com>
2022-03-28 18:00:22 +0200

    feat: basic page to add new simulations

--- src/utils/monaco.rs
+++ src/utils/monaco.rs
@@ -1,3 +1,4 @@
+use crate::utils::to_yaml;
 use monaco::api::TextModel;
 use wasm_bindgen::JsValue;
 
@@ -6,10 +7,7 @@ pub fn to_yaml_model<T>(content: &T) -> Result<TextModel, JsValue>
 where
     T: serde::Serialize,
 {
-    let yaml = serde_yaml::to_string(content).unwrap_or_default();
-    let p: &[_] = &['-', '\n', '\r'];
-    let yaml = yaml.trim_start_matches(p);
-    to_model(Some("yaml"), yaml)
+    to_model(Some("yaml"), to_yaml(content))
 }
 
 /// Convert content to TextModel
